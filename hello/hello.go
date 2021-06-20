2bad95a15d161dd1168098f6238635b6f378cadc
Henrique Mentz <henrique-mentz@hotmail.com>
2021-06-20 15:19:11 -0300

    Refactor to use switch to get a language supplied in tests.

--- hello/hello.go
+++ hello/hello.go
@@ -3,11 +3,31 @@ package hello
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const frenchhHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) string {
-	return englishHelloPrefix + name
+func Hello(name, language string) string {
+
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case "french":
+		prefix = frenchhHelloPrefix
+	case "spanish":
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", "english"))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -5,10 +5,28 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	got := Hello("Ike")
-	want := "Hello, Ike"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+
+		got := Hello("Ike", "english")
+		want := "Hello, Ike"
+		assertCorrectMessage(t, got, want)
+
+	})
+
+	t.Run("say 'hello world' when an empty string in supplied", func(t *testing.T) {
+
+		got := Hello("", "spanish")
+		want := "Hola, World"
+		assertCorrectMessage(t, got, want)
+
+	})
+
 }
